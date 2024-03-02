e61df09acb3aad9ec5691ad1e369c9e29f140dd8
Att3r <atter2207@gmail.com>
2024-03-02 16:58:21 +0200

    parandus

--- FightOrFlight_Project/Assets/LevelSelectionMenuManagar.cs
+++ FightOrFlight_Project/Assets/LevelSelectionMenuManagar.cs
@@ -2,23 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSelectionMenuManagar : MonoBehaviour
+public class LevelSelectionMenuManager : MonoBehaviour
 {
     public void OnClickBack()
     {
         this.gameObject.SetActive(false);
     }  
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
