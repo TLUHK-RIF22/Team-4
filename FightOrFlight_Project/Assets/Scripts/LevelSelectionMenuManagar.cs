da2a5c1622c99d4dbb30b122f8f67102a9ce5c9b
Att3r <atter2207@gmail.com>
2024-03-02 17:52:56 +0200

    level

--- FightOrFlight_Project/Assets/Scripts/LevelSelectionMenuManagar.cs
+++ FightOrFlight_Project/Assets/Scripts/LevelSelectionMenuManagar.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectionMenuManagar : MonoBehaviour
-{
+{   
+    public static int currLevel;
+    public void OnClickLevel(int levelNum)
+    {
+        currLevel = levelNum;
+        SceneManager.LoadScene("SamplaScene");
+    }
     public void OnClickback()
     {
         this.gameObject.SetActive(false);
