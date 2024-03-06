c2c03212560d298bcec8f78a55d8e5910fe10377
anderopib <122923734+anderopib@users.noreply.github.com>
2024-03-06 14:53:05 +0200

    Combat system test
    If Nugis get player then  start a level from beginning. When player jumps on nugis then nugis is stunned.

--- FightOrFlight_Project/Assets/Scripts/PlayerMovement.cs
+++ FightOrFlight_Project/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -78,6 +79,7 @@ public class PlayerMovement : MonoBehaviour
             Destroy(other.gameObject);
             cm.coinCount++;
         }
+  
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -89,4 +91,26 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-}
+        void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Nugis"))
+        {
+            HandleEnemyCollision(collision.transform);
+        }
+    }
+    	private void HandleEnemyCollision(Transform enemy)
+    {
+        // Simple check: if player's bottom is above enemy's top, it's a "jump on top"
+        if (transform.position.y > enemy.position.y + (enemy.GetComponent<Collider2D>().bounds.size.y / 2))
+        {
+            // Logic to stun the enemy
+             enemy.GetComponent<NewBehaviourScript>().Stun();
+        }
+        else
+        {
+            // Logic to reset the player or the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    }
+
