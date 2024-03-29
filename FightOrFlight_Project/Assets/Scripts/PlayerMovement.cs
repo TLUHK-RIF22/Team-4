f74a7dbeca70e5122f1b537352fa2d783e58edce
Jaagup Vapper <jaagup.vapper@gmail.com>
2024-03-29 20:21:08 +0200

    Tree fix

--- FightOrFlight_Project/Assets/Scripts/PlayerMovement.cs
+++ FightOrFlight_Project/Assets/Scripts/PlayerMovement.cs
@@ -310,7 +310,7 @@ public class PlayerMovement : MonoBehaviour
         rb.gravityScale = gravity;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.tag == "Tree")
         {
