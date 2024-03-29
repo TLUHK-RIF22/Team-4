af21a5a4d56958aa097be20836d411e34fc5285e
Jaagup Vapper <jaagup.vapper@gmail.com>
2024-03-29 21:09:02 +0200

    tree prefabs + more level1

--- FightOrFlight_Project/Assets/Scripts/CollectibleManager.cs
+++ FightOrFlight_Project/Assets/Scripts/CollectibleManager.cs
@@ -8,15 +8,25 @@ public class CollectibleManager : MonoBehaviour
     public HealthManager healthManager;
     [SerializeField] private float leafSpeedBoost = 9f;
     [SerializeField] private float leafSpeedBoostDuration = 5f;
+    private bool speedBoostActive = false;
     public void CollectLeaf()
     {
         // Increase player's movement speed
-
-        StartCoroutine(playerMovement.AddSpeedBoost(leafSpeedBoost, leafSpeedBoostDuration));
+        if (!speedBoostActive) {
+            StartCoroutine(playerMovement.AddSpeedBoost(leafSpeedBoost, leafSpeedBoostDuration));
+            speedBoostActive = true;
+            StartCoroutine(SpeedBoostTimer(leafSpeedBoostDuration));
+        }
     }
 
     public void CollectAcorn()
     {
         healthManager.GainHearts();
+    }
+
+    private IEnumerator SpeedBoostTimer(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        speedBoostActive = false;
     }
 }
