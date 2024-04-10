59f5e270dcd367e9e98aecf20c04ff5347165fba
Jaagup Vapper <jaagup.vapper@gmail.com>
2024-04-10 13:58:12 +0300

    Camera x clamping

--- FightOrFlight_Project/Assets/Scripts/CameraController.cs
+++ FightOrFlight_Project/Assets/Scripts/CameraController.cs
@@ -7,11 +7,47 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float minY;
     [SerializeField] private float maxY;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    private float minYEdge;
+    private float maxYEdge;
+    private float minXEdge;
+    private float maxXEdge;
+    private float cameraSize;
+
 
     // Update is called once per frame
     void Update()
     {
         float clampedY = Mathf.Clamp(player.position.y, minY, maxY);
-        this.transform.position = new Vector3(player.position.x, clampedY, this.transform.position.z);
+        float clampedX = Mathf.Clamp(player.position.x, minX, maxX);
+        this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        Gizmos.DrawLine(new Vector3(minXEdge, minYEdge, 0), new Vector3(maxXEdge, minYEdge, 0));
+        Gizmos.DrawLine(new Vector3(maxXEdge, minYEdge, 0), new Vector3(maxXEdge, maxYEdge, 0));
+        Gizmos.DrawLine(new Vector3(maxXEdge, maxYEdge, 0), new Vector3(minXEdge, maxYEdge, 0));
+        Gizmos.DrawLine(new Vector3(minXEdge, maxYEdge, 0), new Vector3(minXEdge, minYEdge, 0));
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 1f);
+        Gizmos.DrawLine(new Vector3(minXEdge, minYEdge, 0), new Vector3(maxXEdge, minYEdge, 0));
+        Gizmos.DrawLine(new Vector3(maxXEdge, minYEdge, 0), new Vector3(maxXEdge, maxYEdge, 0));
+        Gizmos.DrawLine(new Vector3(maxXEdge, maxYEdge, 0), new Vector3(minXEdge, maxYEdge, 0));
+        Gizmos.DrawLine(new Vector3(minXEdge, maxYEdge, 0), new Vector3(minXEdge, minYEdge, 0));
+    }
+
+    void OnValidate()
+    {
+        cameraSize = Camera.main.orthographicSize;
+        minYEdge = minY - cameraSize;
+        maxYEdge = maxY + cameraSize;
+        minXEdge = minX - cameraSize * Camera.main.aspect;
+        maxXEdge = maxX + cameraSize * Camera.main.aspect;
+    }   
 }
