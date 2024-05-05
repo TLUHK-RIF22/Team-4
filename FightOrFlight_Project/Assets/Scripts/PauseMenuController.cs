0262e53bebbdc5b2048bc9f93745e0812e9ed771
anderopib <122923734+anderopib@users.noreply.github.com>
2024-05-05 21:37:47 +0300

    Adding paus menu
    Adding paus menu, resume , menu and quit

---
+++ FightOrFlight_Project/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+void Start()
+{
+    pauseMenuUI.SetActive(false);  
+    isPaused = false;              
+}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        // Load your menu scene here
+        
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("MenuScene");
+    }
+
+public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+
+        // If running in the Unity editor, log quitting for clarity
+        #if UNITY_EDITOR
+        Debug.Log("Game is exiting...");
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
